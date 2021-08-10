9eae4edd39b1bc43423652b807b9024558a542a4
OJTang <oliverwunderlich13@gmail.com>
2021-08-09 19:49:04 -0500

    initial commit

---
+++ demo/src/test/java/com/library/LiftoffLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
