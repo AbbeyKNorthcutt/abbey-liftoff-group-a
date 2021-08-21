bfb4485be99ac32ce8eea7ad6e6668a68ce4dcfc
OJTang <oliverwunderlich13@gmail.com>
2021-08-21 17:32:57 -0500

    Added Search result counter to search results page

--- src/main/resources/static/js/bootstrap.js
+++ src/main/resources/static/js/bootstrap.js
@@ -5,35 +5,33 @@ function searchTermStorage() {
 
       localStorage.setItem("searchTerm", input);
 
+
  }
 
  function search() {
-
+      let bookCount = 0;
 
       fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + localStorage.getItem("searchTerm")+"&maxResults=40&startIndex=0")
               .then(a =>a.json())
               .then(response =>{
 
+
+
                  for(let i=0;i<response.items.length;i++) {
                     let item = response.items[i];
-
-                    //document.getElementById("list-output").innerHTML+="<h2>"+response.items[i];
+                    bookCount++;
                     document.getElementById("list-output").innerHTML +=
                             "<div>"+
                             "<br"+"<b><img src=" + item.volumeInfo.imageLinks.thumbnail + "<br>"+
                             "<br>" + "<b>Title: </b>" + item.volumeInfo.title + "<br>" +
                             "<b>Author: </b>" + item.volumeInfo.authors + "<br>" +
                             "<b>Published Date: </b>" + item.volumeInfo.publishedDate + "<br>" +
-                            "<b>Description: </b>" + item.volumeInfo.description + "<br>"
+                            "<b>Description: </b>" + item.volumeInfo.description.slice(0, 200) +                              "..." + "<br>"
                             +"</div>"+"<br>";
+                            document.getElementById("searchResultNumber").innerHTML = "Search Results: " + bookCount;
                  }
+
+
               })
 
    }
-
-
-   // JQuery to show selected category in dropdown menu
-   $(".dropdown-menu li a").click(function(){
-     var selText = $(this).text();
-     $(this).parents('.dropdown').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-   });
