063e0bdfa94c89a87a19c76cec157a67614dc597
Christina Varghese <christina.varghese@gmail.com>
2021-08-30 12:19:32 -0500

    Adding register.html, login.html to hold those forms for the user to interact with the website. Still mapping to be done within the AuthenticationController.

--- src/main/java/com/lib/models/AbstractEntity.java
+++ src/main/java/com/lib/models/AbstractEntity.java
@@ -3,6 +3,7 @@ package com.lib.models;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
@@ -13,6 +14,19 @@ public abstract class AbstractEntity {
 
     public int getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
 }
